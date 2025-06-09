ceaefe1c3f41fd3797cb44e9327f833af5926c67
Rafi Ananta <148605708+Rayakoo@users.noreply.github.com>
2025-06-09 22:02:19 +0700

    metadata fix, google analytic and GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/common/main-navbar";
 import Footer from "@/components/common/footer";
 import WhatsappBtn from "@/components/common/whatsapp-btn";
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import Script from "next/script";
 
 
 
@@ -19,14 +21,16 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "Riendys Advertising",
+    default: "Riendys Advertising - Neonbox & Acrylic Malang",
     template: "%s | Riendys Advertising",
   },
-  description: "Riendys Advertising adalah perusahaan yang berfokus pada solusi promosi dan branding untuk berbagai usaha. menyediakan neonbox , laser cut , furniture , exterior dan interior.",
+  description:
+    "Riendys Advertising adalah perusahaan yang berfokus pada solusi branding dan promosi. Kami menyediakan neonbox, acrylic, laser cut, furniture, serta desain interior dan eksterior di Malang.",
+  keywords: ["neonbox", "acrylic", "laser cut", "branding", "Malang", "advertising"],
   openGraph: {
-    title: "Riendys Advertising",
+    title: "Riendys Advertising - Neonbox & Acrylic Malang",
     description:
-      "Riendys Advertising adalah perusahaan yang berfokus pada solusi promosi dan branding untuk berbagai usaha. menyediakan neonbox , laser cut , furniture , exterior dan interior.",
+      "Riendys Advertising menyediakan jasa neonbox, acrylic, laser cut, dan branding terbaik di Malang. Hubungi kami untuk solusi promosi yang efektif!",
     url: "https://riendysadvertising.com",
     siteName: "Riendys Advertising",
     images: [
@@ -34,13 +38,21 @@ export const metadata: Metadata = {
         url: "/logo.jpg",
         width: 1200,
         height: 630,
-        alt: "Riendys Advertising",
+        alt: "Riendys Advertising - Neonbox & Acrylic Malang",
       },
     ],
-    locale: "en_US",
+    locale: "id_ID",
     type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Riendys Advertising - Neonbox & Acrylic Malang",
+    description:
+      "Riendys Advertising adalah solusi terbaik untuk branding dan promosi bisnis Anda di Malang. Kami menyediakan neonbox, acrylic, laser cut, dan desain interior.",
+    images: ["/logo.jpg"],
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -49,9 +61,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-KSNBCMGZ" />
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KSNBCMGZ');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KSNBCMGZ"
+            style={{ display: "none", visibility: "hidden", height: 0, width: 0 }}
+          ></iframe>
+        </noscript>
         <Navbar />
         
        
@@ -59,6 +94,7 @@ export default function RootLayout({
         <WhatsappBtn />
         <Footer />
       </body>
+       <GoogleAnalytics gaId="G-G6S12V5XJW" />
     </html>
   );
 }
